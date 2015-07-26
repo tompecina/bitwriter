0d477a8e6018d47dce5b2267d7fd8d1598e78664
Tomáš Pecina <tomas@pecina.cz>
2015-07-26 17:45:56 +0200

    TestProcess simplified

--- test/src/cz/pecina/bin/bitwriter/TestProcessor.java
+++ test/src/cz/pecina/bin/bitwriter/TestProcessor.java
@@ -1,23 +1,20 @@
 package cz.pecina.bin.bitwriter;
 
-import java.net.URL;
-import java.net.URISyntaxException;
+import java.util.List;
+import java.util.ArrayList;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 import java.util.regex.MatchResult;
 import java.io.File;
 import java.io.InputStream;
-import java.io.ByteArrayInputStream;
-import java.io.FileReader;
-import java.io.BufferedReader;
-import java.io.OutputStream;
+import java.io.StreamTokenizer;
+import java.io.InputStreamReader;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import junit.framework.TestCase;
 
 public class TestProcessor extends TestCase {
 
-    protected URL crcFileURL;
     protected PresetCrcModels presetCrcModels;
     protected Parameters parameters;
 
@@ -25,68 +22,32 @@ public class TestProcessor extends TestCase {
     protected void setUp() throws PresetCrcModelsException,
 				  ParametersException,
 				  IOException {
-	crcFileURL = BitWriter.class.getResource("crc.xml");
 	presetCrcModels = new PresetCrcModels(
 	    BitWriter.class.getResourceAsStream("crc.xml"));
 	parameters = new Parameters();
     }
 
-    private String resourceToString(final String name
-				    ) throws IOException {
-	BufferedReader reader;
-	try {
-	    reader = new BufferedReader(
-	        new FileReader(
-		    new File(getClass().getResource(name).toURI())));
-	} catch (NullPointerException |
-		 IOException |
-		 URISyntaxException exception) {
-	    System.err.println(name);
-	    return null;
-	}
-	final StringBuilder string = new StringBuilder();
-	while (true) {
-	    String s;
-	    try {
-		s = reader.readLine();
-	    } catch (IOException exception) {
-		break;
-	    }
-	    if (s == null) {
-		break;
-	    }
-	    string.append(s + "\n");
-	}
-	reader.close();
-	return string.toString();
-    }
-	
     private boolean testFile(final String fileName) {
 	final String inPrefix = "input/";
 	final String outPrefix = "output/";
-	String outputString = null;
+	final StreamTokenizer outputTokenizer =
+	    new StreamTokenizer(new InputStreamReader(
+	    getClass().getResourceAsStream((outPrefix + fileName)
+	    .replace(".xml", ".hex"))));
+	outputTokenizer.resetSyntax();
+        outputTokenizer.wordChars('a', 'f');
+        outputTokenizer.wordChars('A', 'F');
+        outputTokenizer.wordChars('0', '9');
+        outputTokenizer.whitespaceChars(0, ' ');
+	final List<Integer> outputBytes = new ArrayList<>();
 	try {
-	    outputString = resourceToString(
-	        (outPrefix + fileName).replace(".xml", ".hex"));
-	} catch (IOException exception) {
-	    fail("Failed to process file '" + (outPrefix + fileName) + "'");
-	}
-	int outputLength = 0;
-	byte[] outputBytes = null;
-	if (outputString.length() > 0) {
-	    final String [] outputStringArray =
-		outputString.trim().split("\\s+");
-	    outputLength = outputStringArray.length;
-	    if (outputStringArray[0].length() == 0) {
-		outputLength = 0;
-	    } else {
-		outputBytes = new byte[outputLength];
-		for (int i = 0; i < outputLength; i++) {
-		    outputBytes[i] = (byte)(Integer.valueOf(
-		        outputStringArray[i], 16) & 0xff);
-		}
+	    while (outputTokenizer.nextToken() != StreamTokenizer.TT_EOF) {
+		outputBytes.add(Integer.valueOf(outputTokenizer.sval, 16));
 	    }
+	} catch (IOException exception) {
+	    fail("Failed to tokenize file '" + (outPrefix + fileName) + "'");
 	}
+	final int outputLength = outputBytes.size();
 	final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 	try (InputStream inputStream = getClass()
 	     .getResourceAsStream(inPrefix + fileName)) {
@@ -103,7 +64,7 @@ public class TestProcessor extends TestCase {
 	    return false;
 	}
 	for (int i = 0; i < outputLength; i++) {
-	    if (outputByteArray[i] != outputBytes[i]) {
+	    if ((outputByteArray[i] & 0xff) != outputBytes.get(i)) {
 	    	return false;
 	    }
 	}
@@ -154,7 +115,6 @@ public class TestProcessor extends TestCase {
     }
     
     public void testInternalModels() {
-	assertNotNull("crcFileURL is null", crcFileURL);
 	assertNotNull("models is null", presetCrcModels);
     }
 
