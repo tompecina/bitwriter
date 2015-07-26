6bc68eca2a55a77e6738755671329858b799cf67
Tomáš Pecina <tomas@pecina.cz>
2015-07-26 13:36:27 +0200

    minor fix

--- src/cz/pecina/bin/bitwriter/ParsedElement.java
+++ src/cz/pecina/bin/bitwriter/ParsedElement.java
@@ -26,9 +26,8 @@ import java.util.List;
 import java.util.ArrayList;
 import java.io.IOException;
 import org.w3c.dom.Node;
-import org.w3c.dom.Element;
-import org.w3c.dom.Text;
 import org.w3c.dom.NodeList;
+import org.w3c.dom.Element;
 import java.util.logging.Logger;
 
 /**
